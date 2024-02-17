feb7bd9f9af9941d8bd77fbae4d10b021fd9cf67
M-Taguchi <ponpocori.chiccyorina@gmail.com>
2024-02-17 03:51:59 +0000

    フォント読み込み

--- src/resetCss.ts
+++ src/resetCss.ts
@@ -1,4 +1,11 @@
 import { css } from 'hono/css'
 export const resetCss = css`
 *,*::before,*::after{box-sizing:border-box}html{-moz-text-size-adjust:none;-webkit-text-size-adjust:none;text-size-adjust:none}body,h1,h2,h3,h4,p,figure,blockquote,dl,dd{margin:0}ul[role='list'],ol[role='list']{list-style:none}body{min-height:100vh;line-height:1.5}h1,h2,h3,h4,button,input,label{line-height:1.1}h1,h2,h3,h4{text-wrap:balance}a:not([class]){text-decoration-skip-ink:auto;color:currentColor}img,picture{max-width:100%;display:block}input,button,textarea,select{font:inherit}textarea:not([rows]){min-height:10em}:target{scroll-margin-block:5ex}
+@font-face {
+  font-family: NikumaruFont;
+  src: url(${import.meta.env.PROD ? "." : "public"}/nikumaru.otf);
+}
+body {
+  font-family: NikumaruFont;
+}
 `
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,6 @@ import devServer from '@hono/vite-dev-server'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // エイリアスの設定
-  resolve: {
-    alias: {
-      "public": "/public",
-    }
-  },
   plugins: [
     ssg(),
     devServer({
